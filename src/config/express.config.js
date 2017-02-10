5e95b00b004869c8d4f114dd35a67157fb09b05b
Raul F. de Melo <melo.raulf@gmail.com>
2017-02-10 17:59:19 -0200

    Identação

--- src/config/express.config.js
+++ src/config/express.config.js
@@ -2,10 +2,10 @@ const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
 
-module.exports = function() {
+module.exports = function () {
     const app = express();
 
-    app.use(function(req, res, next) {
+    app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
@@ -13,7 +13,7 @@ module.exports = function() {
 
     app.use(bodyParser.json());
 
-    consign({ cwd: 'src' })
+    consign({cwd: 'src'})
         .include('api')
         .then('routes')
         .into(app);
