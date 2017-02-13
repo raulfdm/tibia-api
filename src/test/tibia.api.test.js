cd29a4d284a3dc1f9ae31c7d0de091911da25fb4
Raul F. de Melo <melo.raulf@gmail.com>
2017-02-12 23:44:17 -0200

    Adicionada lib de teste automatizado

---
+++ src/test/tibia.api.test.js
@@ -0,0 +1,14 @@
+const TibiaAPI = require('../api/tibia.api');
+
+test('validateParameter equals null', () => {
+    return TibiaAPI
+        .validateParameter()
+        .catch(err => expect(err).toThrow(Error))
+})
+test('validateParameter with some number', () => {
+
+    let param = "raul1";
+    return TibiaAPI
+        .validateParameter(param)
+        .catch(err => expect(err).toThrow())
+})
