49024a0872b290c7f84e3cf4a9e8d96265d9235e
Raul F. de Melo <melo.raulf@gmail.com>
2017-02-12 23:45:01 -0200

    Alterado o método de get pra post
    Agora o erro obtido na aplicação, é retornado para o usuário

--- src/routes/tibia.route.js
+++ src/routes/tibia.route.js
@@ -2,15 +2,13 @@ const TibiaAPI = require('../api/tibia.api');
 
 module.exports = app => {
 
-    app
-        .get('/', function (req, res) {
-            TibiaAPI
-                .validateParameter(req.query)
-                .then(characterName => TibiaAPI.getCharacterName(characterName))
-                .then(parameterNameForGet => TibiaAPI.consultSite(app, parameterNameForGet))
-                .then(htmlResponse => TibiaAPI.createCharacterObject(htmlResponse))
-                .then(character => res.json(character))
-                .catch(err => console.log(err));
-
-        });
+    app.post('/', (req, res) => {
+        TibiaAPI
+            .validateParameter(req.body)
+            .then(characterName => TibiaAPI.getCharacterName(characterName))
+            .then(parameterNameForGet => TibiaAPI.consultSite(app, parameterNameForGet))
+            .then(htmlResponse => TibiaAPI.createCharacterObject(htmlResponse))
+            .then(character => res.json(character))
+            .catch(err => res.status(400).json({error: err.message}));
+    })
 }
