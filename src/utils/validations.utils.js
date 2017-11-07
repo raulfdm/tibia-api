08af9c3c2e158e9a8f86e79bf3a3cee6c1e6462a
Raul Felipe de Melo <melo.raulf@gmail.com>
2017-11-07 15:00:38 -0200

    Wrote validations tests

--- src/utils/validations.utils.js
+++ src/utils/validations.utils.js
@@ -1,6 +1,9 @@
 const hasSpecificLength = ({ target, length }) => {
   if (target === undefined) throw new Error('Target is required')
+  if (typeof target !== 'string') throw new Error('Target must be a string')
   if (length === undefined) throw new Error('Length is required')
+  if (typeof length !== 'number') throw new Error('Lenght must be a number')
+
   return target.length === length
 }
 
